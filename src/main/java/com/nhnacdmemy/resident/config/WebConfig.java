9fae638c53f0e02befd3405691983850d9f8072b
{GaHyoung} <[mnb2457@naver.com](mailto:mnb2457@naver.com)>
2024-02-16 22:08:41 +0900

    Thymeleaf로 index 실행 성공

--- src/main/java/com/nhnacdmemy/resident/config/WebConfig.java
+++ src/main/java/com/nhnacdmemy/resident/config/WebConfig.java
@@ -1,12 +1,85 @@
 package com.nhnacdmemy.resident.config;
 
 import com.nhnacdmemy.resident.controller.ControllerBase;
+import org.springframework.beans.BeansException;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring5.SpringTemplateEngine;
+import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
+import org.thymeleaf.spring5.view.ThymeleafViewResolver;
+import org.thymeleaf.templatemode.TemplateMode;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackageClasses = ControllerBase.class)
-public class WebConfig {
+public class WebConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+    private ApplicationContext applicationContext;
+    @Override
+    public void setApplicationContext(ApplicationContext applicationContext) throws BeansException {
+        this.applicationContext = applicationContext;
+    }
+
+    /**
+     * Thymeleaf 뷰 리졸버 설정.
+     * */
+    @Bean
+    public ThymeleafViewResolver thymeleafViewResolver(){
+        ThymeleafViewResolver thymeleafViewResolver = new ThymeleafViewResolver();
+        thymeleafViewResolver.setTemplateEngine(springTemplateEngine());
+        thymeleafViewResolver.setCharacterEncoding("UTF-8");
+        thymeleafViewResolver.setOrder(1);
+        thymeleafViewResolver.setViewNames(new String[]{"*"});
+        return thymeleafViewResolver;
+    }
+
+    /**
+     * Thymeleaf 템플릿 엔진 설정.
+     * 템플릿엔진 : 템플릿을 사용하여 동적으로 문서 형식을 생성하는 데 사용되는 소프트웨어
+     * */
+    public SpringTemplateEngine springTemplateEngine(){
+        SpringTemplateEngine templateEngine = new SpringTemplateEngine();
+        templateEngine.setTemplateResolver(springResourceTemplateResolver());
+        return templateEngine;
+    }
+
+    /**
+     * 스프링 리소스 템플릿 리졸버 설정.
+     * 템플릿 파일의 위치를 지정하고, 해당 위치에서 템플릿 파일을 로드하여 처리
+     * */
+    public SpringResourceTemplateResolver springResourceTemplateResolver(){
+        SpringResourceTemplateResolver springResourceTemplateResolver = new SpringResourceTemplateResolver();
+        springResourceTemplateResolver.setApplicationContext(applicationContext);
+        springResourceTemplateResolver.setCharacterEncoding("UTF-8");
+        springResourceTemplateResolver.setPrefix("/WEB-INF/views/");
+        springResourceTemplateResolver.setSuffix(".html");
+        springResourceTemplateResolver.setCacheable(false);
+        springResourceTemplateResolver.setTemplateMode(TemplateMode.HTML);
+        return springResourceTemplateResolver;
+    }
+
+    /**
+     * 뷰 리졸버 설정을 구성.
+     * 컨트롤러가 반환한 뷰 이름을 실제 뷰로 매핑
+     * */
+    @Override
+    public void configureViewResolvers(ViewResolverRegistry registry) {
+        registry.viewResolver(thymeleafViewResolver());
+    }
+
+    /**
+     * 뷰 컨트롤러를 추가.
+     * */
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("index");
+        registry.addViewController("/index.html").setViewName("index");
+    }
 }
